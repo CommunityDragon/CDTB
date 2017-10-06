0ed23ad1b1fee9c1b587f9b030b69097767bd1bc
Xxmagic101 <imamnothere@users.noreply.github.com>
2017-10-05 21:07:00 -0400

    Main Program

--- LoLPatcher/src/nl/xupwup/Util/RoundFloat.java
+++
@@ -1,7 +0,0 @@
-package nl.xupwup.Util;
-
-public class RoundFloat {
-    public static double round(double d, int decimals){
-        return Math.round(d * Math.pow(10, decimals)) / Math.pow(10, decimals);
-    }
-}
