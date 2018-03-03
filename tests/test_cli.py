8dbd248e0d3fd8ad2bc45ee9558ba4f51e9237be
Benoît Ryder <benoit@ryder.fr>
2018-03-03 23:00:40 +0100

    Update automatic tests, following removal of 'export -u'

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -32,13 +32,12 @@ def runner(tmpdir):
     return _runner
 
 
-@pytest.mark.parametrize("args, version, previous_version, overwrite", [
-    ("7.24", '7.24', '7.23', True),
-    ("7.24 --previous 7.22", '7.24', '7.22', True),
-    ("7.24 --full", '7.24', None, True),
-    ("7.24 -u", '7.24', '7.23', False),
+@pytest.mark.parametrize("args, version, previous_version", [
+    ("7.24", '7.24', '7.23'),
+    ("7.24 --previous 7.22", '7.24', '7.22'),
+    ("7.24 --full", '7.24', None),
 ])
-def test_cli_export_versions(runner, monkeypatch, mocker, args, version, previous_version, overwrite):
+def test_cli_export_versions(runner, monkeypatch, mocker, args, version, previous_version):
     def patch_versions(storage, stored=False):
         for v in ('7.25', '7.24', '7.23', '7.22'):
             yield PatchVersion(storage, Version(v), [])
@@ -53,7 +52,7 @@ def test_cli_export_versions(runner, monkeypatch, mocker, args, version, previou
         previous_patch = None if previous_version is None else PatchVersion(None, previous_version, [])
         mock.assert_called_once_with(os.path.join('export', '7.24'), patch, previous_patch)
 
-        mock_instance.export.assert_called_once_with(overwrite=overwrite)
+        mock_instance.export.assert_called_once_with()
         mock_instance.write_links.assert_called_once_with()
 
 @pytest.mark.parametrize("arg_storage, arg_cdn, env_storage, env_cdn, path, url", [
