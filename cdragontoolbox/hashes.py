40f065870f5d3bec99aa73d6dd5f3be8e72139db
Moritz Bender <35152647+Morilli@users.noreply.github.com>
2020-10-30 22:47:29 +0100

    Add support for .metal shader file guessing (#42)

--- cdragontoolbox/hashes.py
+++ cdragontoolbox/hashes.py
@@ -722,15 +722,17 @@ class GameHashGuesser(HashGuesser):
     def guess_shader_variants(self):
         """Guess different extension variants for shader files, e.g. ".glsl_100" """
 
-        shader_paths = [path for path in self.known.values() if path.endswith(".ps_2_0") or path.endswith(".vs_2_0")]
-
+        shader_extensions = [f".{variant}s_{n}_0" for variant in "pv" for n in "23"]
+        shader_paths = [path for path in self.known.values() if os.path.splitext(path)[-1] in shader_extensions]
         for path in shader_paths:
             self.check(f"{path}.dx9")
             self.check(f"{path}.dx11")
             self.check(f"{path}.glsl")
+            self.check(f"{path}.metal")
             self.check_iter(f"{path}.dx9_{n}" for n in range(0, 100000, 100))
             self.check_iter(f"{path}.dx11_{n}" for n in range(0, 100000, 100))
             self.check_iter(f"{path}.glsl_{n}" for n in range(0, 100000, 100))
+            self.check_iter(f"{path}.metal_{n}" for n in range(0, 100000, 100))
 
     def grep_wad(self, wad):
         """Find hashes from a wad file"""
