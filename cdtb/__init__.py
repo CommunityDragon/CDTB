dd10af96593a43e25bfbd81217a4e4a7163b6f2b
Benoît Ryder <benoit@ryder.fr>
2024-09-01 16:03:53 +0200

    Remove re-exports from cdtb root package
    Those are old and have never be updated, and are probably unused.
    This breaks compatibility but is easy to fix.

--- cdtb/__init__.py
+++ cdtb/__init__.py
@@ -1,20 +1,2 @@
 import logging
 logger = logging.getLogger(__name__)
-
-from cdtb.storage import (
-    Storage,
-    Patch,
-    PatchElement,
-)
-from cdtb.wad import (
-    Wad,
-)
-from cdtb.export import (
-    Exporter,
-    CdragonRawPatchExporter,
-)
-
-# import storages to register them
-import cdtb.rads
-import cdtb.patcher
-
